b146c7fad3eb707b9216a25a3197c90c710a9f9b
masterbaco <juanvelasquez.c@gmail.com>
2015-09-09 23:04:16 -0500

    Add PostgreSQL as production database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,7 @@ Rails.application.configure do
 
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
-  port:                 465,
-  tls:                   true, 
+  port:                 587, 
   domain:               'example.com',
   user_name:            ENV['GMAIL_USERNAME'],
   password:             ENV['GMAIL_PASSWORD'],
